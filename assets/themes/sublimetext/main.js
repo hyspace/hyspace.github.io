ef3e677861d43eb73c76b1e61875ee62d9787cd9
hyspace <hyspace@gmail.com>
2013-10-22 20:36:57 +0800

    no message

--- assets/themes/sublimetext/main.js
+++ assets/themes/sublimetext/main.js
@@ -71,6 +71,6 @@ $(function(){
     $minimapHandler.height(sh1);
   }
   $( window ).resize(resize);
-  body.onload=resize;
+  document.body.onload=resize;
 })
 console.log('I already have a grilfriend.');
